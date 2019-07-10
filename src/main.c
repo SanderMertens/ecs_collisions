7753d72c54b6d8dcf4db044583ee7d97c1649dc0
Sander Mertens <sander.mertens8@gmail.com>
2019-07-09 21:16:53 -0700

    Use ECS_COLUMN, new inheritance API

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include <ecs_collisions.h>
 
 void SetColor(ecs_rows_t *rows) {
-    EcsCollision2D *collision = ecs_column(rows, EcsCollision2D, 1);
-    ecs_type_t TEcsColor = ecs_column_type(rows, 2);
+    ECS_COLUMN(rows, EcsCollision2D, collision, 1);
+    ECS_COLUMN_COMPONENT(rows, EcsColor, 2);
 
     for (int i = 0; i < rows->count; i ++) {
         /* Set or override (if collided for the first time) the entity color */
@@ -12,12 +12,12 @@ void SetColor(ecs_rows_t *rows) {
 }
 
 void ResetColor(ecs_rows_t *rows) {
-    /* Little trick to ensure the color is only set if entity owns EcsColor:
-     * ecs_column_test returns NULL if the column is a shared component. */
-    EcsColor *color = ecs_column_test(rows, EcsColor, 1);
+    ECS_COLUMN(rows, EcsColor, color, 1);
 
-    for (int i = 0; color && i < rows->count; i ++) {
-        color[i] = (EcsColor){color[i].r * 0.97, 50, 100, 255}; /* Gradually fade to blue */
+    if (color && !ecs_is_shared(rows, 1)) {
+        for (int i = 0; color && i < rows->count; i ++) {
+            color[i] = (EcsColor){color[i].r * 0.97, 50, 100, 255}; /* Fade to blue */
+        }
     }
 }
 
@@ -33,15 +33,15 @@ int main(int argc, char *argv[]) {
 
     /* Define prefabs for collidable circles and squares with a color */
     ECS_PREFAB(world, Shape, EcsColor, EcsCollider);
-    ECS_PREFAB(world, Circle, Shape, EcsCircle);
-    ECS_PREFAB(world, Square, Shape, EcsSquare);
+    ECS_PREFAB(world, Circle, INSTANCEOF | Shape, EcsCircle);
+    ECS_PREFAB(world, Square, INSTANCEOF | Shape, EcsSquare);
     ecs_set(world, Shape, EcsColor, {0, 50, 100, 255}); /* Initially shapes all share the same color */
     ecs_set(world, Circle, EcsCircle, {.radius = 24});
     ecs_set(world, Square, EcsSquare, {.size = 24});
 
     /* Systems that implement logic for coloring entities */
     ECS_SYSTEM(world, ResetColor, EcsOnUpdate, EcsColor);
-    ECS_SYSTEM(world, SetColor, EcsOnSet, EcsCollision2D, ID.EcsColor);
+    ECS_SYSTEM(world, SetColor, EcsOnSet, EcsCollision2D, .EcsColor);
 
     /* Create canvas (used by SDL to create window) */
     ecs_set(world, 0, EcsCanvas2D, { .window = {.width = 800, .height = 600} });
@@ -57,13 +57,14 @@ int main(int argc, char *argv[]) {
        {-100, -50}, {-100, 0}, {-100, 50}, {100, -50}, {100, 0}, {100, 50}};
 
     for (int i = 0; i < 16; i ++) {
-        e = _ecs_new(world, i < 4 ? TCircle : TSquare);
+        e = _ecs_new_instance(world, i < 4 ? Circle : Square, 0);
         ecs_set(world, e, EcsPosition2D, {pos[i].x, pos[i].y});
     }
 
     /* Main loop */
     ecs_set_target_fps(world, 60);
-    while (ecs_progress(world, 0));
+
+    while (ecs_progress(world, 0)) { }
 
     return ecs_fini(world);
 }
